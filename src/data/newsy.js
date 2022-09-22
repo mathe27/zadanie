24056c73e84e09262450e21fd3459846c87e7a69
mathe27 <mateusz.gielmuda@gmail.com>
2022-09-22 21:56:55 +0200

    newsy - dodana tresc

--- src/data/newsy.js
+++ src/data/newsy.js
@@ -3,78 +3,78 @@ export const _newsy = {
         'news1': {
             'title':'Lorem...',
             'img':'https://picsum.photos/200/300',
-            'content':'ipsum...'
+            'content':'Duis imperdiet ante odio, sed faucibus velit tristique nec. Praesent pretium libero lacus, eu vehicula lacus sollicitudin id. Praesent sit amet turpis accumsan, egestas justo non, lobortis mauris. Nulla scelerisque tristique dui. Mauris fermentum magna id dolor dapibus iaculis. Duis et metus lectus. Donec malesuada ante et tincidunt facilisis.'
         },
         'news2': {
             'title':'Lorem2...',
             'img':'/images/annie-spratt-5ABow0uVv_k-unsplash.jpg',
-            'content':'ipsum2...'
+            'content':'Donec mollis dapibus est sed tincidunt. Maecenas ut auctor turpis. Sed egestas mi mauris, vel pharetra lacus bibendum eu. Duis eu leo hendrerit, accumsan dui non, semper justo. '
         },
         'news3': {
             'title':'Lorem.3..',
             'img':'/images/markus-spiske-ujsc723Pn7g-unsplash.jpg',
-            'content':'ipsum3...'
+            'content':'Aenean posuere arcu quis nisl rhoncus accumsan. Donec justo nunc, pharetra ut ultricies in, aliquam quis sapien. Morbi erat augue, dictum in leo laoreet, lacinia porttitor diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae laoreet sapien, eget malesuada enim. Ut eget consectetur massa, eu convallis risus.'
         },
     },
     'kategoria2':{
         'news4': {
             'title':'Lorem...',
             'img':'https://picsum.photos/200/300',
-            'content':'ipsum...'
+            'content':'Donec mollis dapibus est sed tincidunt. Maecenas ut auctor turpis. Sed egestas mi mauris, vel pharetra lacus bibendum eu. Duis eu leo hendrerit, accumsan dui non, semper justo. '
         },
         'news5': {
             'title':'Lorem2...',
             'img':'https://picsum.photos/200/300',
-            'content':'ipsum2...'
+            'content':'Duis imperdiet ante odio, sed faucibus velit tristique nec. Praesent pretium libero lacus, eu vehicula lacus sollicitudin id. Praesent sit amet turpis accumsan, egestas justo non, lobortis mauris. Nulla scelerisque tristique dui. Mauris fermentum magna id dolor dapibus iaculis. Duis et metus lectus. Donec malesuada ante et tincidunt facilisis.'
         },
         'news5': {
             'title':'Lorem.3..',
             'img':'/images/Gull_portrait_ca_usa.jpg',
-            'content':'ipsum3...'
+            'content':'iDuis imperdiet ante odio, sed faucibus velit tristique nec. Praesent pretium libero lacus, eu vehicula lacus sollicitudin id. Praesent sit amet turpis accumsan, egestas justo non, lobortis mauris. Nulla scelerisque tristique dui. Mauris fermentum magna id dolor dapibus iaculis. Duis et metus lectus. Donec malesuada ante et tincidunt facilisis.'
         },
         'news7': {
             'title':'Lorem...',
             'img':'https://picsum.photos/200/300',
-            'content':'ipsum...'
+            'content':'Donec mollis dapibus est sed tincidunt. Maecenas ut auctor turpis. Sed egestas mi mauris, vel pharetra lacus bibendum eu. Duis eu leo hendrerit, accumsan dui non, semper justo. '
         },
         'news8': {
             'title':'Lorem2...',
             'img':'https://picsum.photos/200/300',
-            'content':'ipsum2...'
+            'content':'Aenean posuere arcu quis nisl rhoncus accumsan. Donec justo nunc, pharetra ut ultricies in, aliquam quis sapien. Morbi erat augue, dictum in leo laoreet, lacinia porttitor diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae laoreet sapien, eget malesuada enim. Ut eget consectetur massa, eu convallis risus.'
         },
         'news9': {
             'title':'Lorem.3..',
             'img':'/images/photo-1503023345310-bd7c1de61c7d.jpg',
-            'content':'ipsum3...'
+            'content':'Duis imperdiet ante odio, sed faucibus velit tristique nec. Praesent pretium libero lacus, eu vehicula lacus sollicitudin id. Praesent sit amet turpis accumsan, egestas justo non, lobortis mauris. Nulla scelerisque tristique dui. Mauris fermentum magna id dolor dapibus iaculis. Duis et metus lectus. Donec malesuada ante et tincidunt facilisis.'
         },
     },
     'kategoria3':{
         'news10': {
             'title':'Lorem...',
             'img':'https://picsum.photos/200/300',
-            'content':'ipsum...'
+            'content':'Donec mollis dapibus est sed tincidunt. Maecenas ut auctor turpis. Sed egestas mi mauris, vel pharetra lacus bibendum eu. Duis eu leo hendrerit, accumsan dui non, semper justo. '
         },
         'news11': {
             'title':'Lorem2...',
             'img':'https://picsum.photos/200/300',
-            'content':'ipsum2...'
+            'content':'Suspendisse at molestie ligula, ut dignissim erat. Duis in vehicula leo.'
         },
         'news12': {
             'title':'Lorem.3..',
             'img':'https://picsum.photos/200/300',
-            'content':'ipsum3...'
+            'content':'Nam sed nisi ut lorem volutpat finibus. Integer eget magna magna. Sed purus risus, fermentum at mauris at, pharetra dignissim ex. Mauris auctor, velit quis egestas faucibus, lacus enim pretium purus, eu blandit massa metus at dui. Suspendisse at ornare magna. In nec accumsan ligula. Aenean posuere arcu quis nisl rhoncus accumsan. Donec justo nunc, pharetra ut ultricies in, aliquam quis sapien. Morbi erat augue, dictum in leo laoreet, lacinia porttitor diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae laoreet sapien, eget malesuada enim. Ut eget consectetur massa, eu convallis risus. Duis augue metus, rutrum quis sollicitudin sit amet, dictum nec ligula. '
         },
     },
     'kategoria4':{
         'news13': {
             'title':'Lorem.3..',
             'img':'/images/photo-1503023345310-bd7c1de61c7d.jpg',
-            'content':'ipsum3...'
+            'content':'Pellentesque sit amet est dolor. Phasellus orci turpis, faucibus ac facilisis sit amet, aliquam et lacus. Suspendisse at molestie ligula, ut dignissim erat. Duis in vehicula leo. '
         },
         'news14': {
             'title':'Lorem.aaa..',
             'img':'/images/markus-spiske-ujsc723Pn7g-unsplash.jpg',
-            'content':'ipsum3...'
+            'content':'Suspendisse at molestie ligula, ut dignissim erat. Duis in vehicula leo.'
         },
     }
 };
